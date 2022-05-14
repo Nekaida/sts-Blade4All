c64e9f004b8e8cb1a4b1f135a9ef0edb6af33184
Nekaida <nekaida@nelaco.de>
2022-05-14 19:05:16 +0200

    Clean up

--- src/main/java/blade4all/patches/RelicLibraryPatch.java
+++ src/main/java/blade4all/patches/RelicLibraryPatch.java
@@ -1,29 +1,12 @@
 package blade4all.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.helpers.RelicLibrary;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.relics.WristBlade;
-
-/*@SpirePatch(
-        clz= RelicLibraryPatch.class,
-        method="initialize"
-)
-public class RelicLibraryPatch {
-    public static void Postfix(RelicLibrary lib) {
-        AbstractRelic relic = lib.greenRelics.get("WristBlade");
-        lib.greenRelics.remove(relic.relicId);
-        lib.greenList.remove(relic);
-        lib.sharedRelics.put(relic.relicId, relic);
-        lib.sortLists();
-    }
-}*/
-
-
-
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.WristBlade;
 
 @SpirePatch(clz=RelicLibrary.class, method="initialize")
 public class RelicLibraryPatch {
@@ -32,7 +15,7 @@ public class RelicLibraryPatch {
             @Override
             public void edit(MethodCall m) throws CannotCompileException {
                 if (m.getMethodName().equals("addGreen") && m.getLineNumber() == 200) {
-                    m.replace("add(new com.megacrit.cardcrawl.relics.WristBlade());");
+                    m.replace("add((com.megacrit.cardcrawl.relics.AbstractRelic)new com.megacrit.cardcrawl.relics.WristBlade());");
                 }
             }
         };
